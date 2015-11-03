b4817be56bde67e7ce8786d32f566c1d35dba278
ifels <niexinxin1986@gmail.com>
2015-11-03 21:33:05 +0800

    deploy is ok

--- deploy.py
+++ deploy.py
@@ -9,10 +9,11 @@ import subprocess
 
 
 BLOG_GIT = 'git@github.com:ifels/ifels.blog.git'
+GIHUB_IO_GIT = 'git@github.com:ifels/ifels.github.com.git'
 
 BLOG_NAME = 'ifels.blog'
 
-class ChDir:
+class switchDir:
     """Context manager for changing the current working directory"""
     def __init__(self, new_path):
         self.newPath = os.path.expanduser(new_path)
@@ -33,11 +34,18 @@ def deploy():
     os.system('git clone --recursive %s %s' % (BLOG_GIT, BLOG_NAME))
 
     blogDir = curDir + '/' + BLOG_NAME
+    publicDir = blogDir + '/public'
 
-    with ChDir(blogDir):
+    with switchDir(blogDir):
         s = subprocess.Popen('git log -1 --pretty=format:"%s"', shell=True, stdout=subprocess.PIPE)
         commitMsg = s.communicate()[0]
         print("commitMsg", commitMsg)
+
+        os.system('rm -rf public')
+        os.system('git clone %s %s' % (GIHUB_IO_GIT, 'public'))
+        os.system('hugo -v -b "http://ifels.github.io" -t redlounge')
+
+    with switchDir(publicDir):
         os.system('git add --all')
         os.system('git commit -a -m "%s"' % commitMsg)
         os.system('git push origin master')
